e3eea0dd4f24f83f956d62d1e4743510d49a80ee
Tanner Gaudes <tmg1999@yahoo.com>
2026-05-24 03:05:21 -0500

    Update launcher to version 0.8.2
    Reduce icon size in various windows from 80x80 to 64x64 to ensure error message does not block buttons on bottom.

--- RPG_Launcher/Util/AppData.cs
+++ RPG_Launcher/Util/AppData.cs
@@ -71,7 +71,7 @@ namespace RPG_Launcher.Util
 
 
         // Application version, hard-coded. Publicly-readable Version property is used to read this.
-        private static readonly string version = "0.8.1";
+        private static readonly string version = "0.8.2";
         // Path to appdata.json file (should be working directory).
         private static readonly string appDataPath = "appdata.json";
         // Default path to executable.
